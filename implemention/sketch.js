e499d4a412a1dd6015eb9b021e2b2b65c7a5e4d0
uvsq22101785 <91668135+uvsq22101785@users.noreply.github.com>
2025-01-31 21:12:11 +0100

    Add files via upload

--- implemention/sketch.js
+++ implemention/sketch.js
@@ -1,7 +1,35 @@
-function setup() {
- // put setup code here
+
+var canvas_width = 1920;
+var canvas_height = 1080;
+
+let table;
+
+function preload() {
+  table = loadTable("/data/dataset.csv", "csv", "header");
 }
 
+
+function setup() {
+ // put setup code here
+  console.log(table.getRowCount() + " total rows in table");
+  console.log(table.getColumnCount() + " total columns in table");
+  
+  createCanvas(canvas_width, canvas_height);
+
+  dates = table.getColumn("Date");//ALREADY ORDERED
+  number_of_absents = table.getColumn("Absent_mean_NY");
+  absent_rate = table.getColumn("Absent_rate_mean");
+  number_of_presents = table.getColumn("Present_mean_NY");
+  present_rate = table.getColumn("Present_rate_mean");
+  temperature = table.getColumn("temperature_mean (°C)");
+  precipitation = table.getColumn("precipitation (mm)");
+  description = table.getColumn("description");
+
+}
+
+//ALL DATA ARE EASILY ACCESSIBLE YOU CAN DRAW WHATEVER YOU WANT
 function draw() {
   // put drawing code here
+  background(230);
+  
 }
